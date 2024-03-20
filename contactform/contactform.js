118576aa410b5729ec22d50077ca0cc90f8980b3
ocelyan <celyan.ouali@ynov.com>
2024-03-20 16:35:58 +0100

    fixed ?

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -7,6 +7,7 @@ jQuery(document).ready(function($) {
     var form = $(this); // Reference to the form element
 
     // Define the parameters for emailjs.sendForm
+    emailjs.initiate('yMMakr9KQLEBwbkJb');
     var serviceID = 'service_i58p4dg';
     var templateID = 'template_vnvsnrc';
     var options = {}; // You can optionally specify additional options here
